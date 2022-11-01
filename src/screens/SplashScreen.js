d517bbb20968ff09d1cf6044998b25611d0c4b92
Roja Gone <goneroja711@gmail.com>
2022-11-01 15:51:20 +0530

    splash screen update

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,71 +1,27 @@
-import React, { useState } from 'react';
-import { StyleSheet, TextInput, View, TouchableOpacity } from 'react-native';
-import Icon1 from 'react-native-vector-icons/Entypo';
+import React from 'react';
+import {View, Text, StyleSheet, } from 'react-native';
 
-export default function SplashScreen() {
-  const [password, setPassword] = useState('');
-  const [passwordVisibility, setPasswordVisibility] = useState(true);
-  const [rightIcon, setRightIcon] = useState('eye-with-line');
-
-  const handlePasswordVisibility = () => {
-    if (rightIcon === 'eye') {
-      setRightIcon('eye-with-line');
-      setPasswordVisibility(!passwordVisibility);
-    } else if (rightIcon === 'eye-with-line') {
-      setRightIcon('eye');
-      setPasswordVisibility(!passwordVisibility);
-    }
-  };
-
+const LoaderScreen = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.inputField}
-          name="password"
-          placeholder="Enter password"
-          autoCapitalize="none"
-          autoCorrect={false}
-          textContentType="newPassword"
-          value={password}
-          enablesReturnKeyAutomatically
-          onChangeText={text => setPassword(text)}
-          secureTextEntry={passwordVisibility}
-        />
-         <TouchableOpacity onPress={handlePasswordVisibility}>
-            <Icon1 name={rightIcon}
-          size={25}
-          color="#121110"
-          style={styles.iconStyle}
-
-          />
-        </TouchableOpacity>
-      </View>
+    <View style={styles.Container}>
+ 
+      <Text style={styles.Logo}>Together</Text>
     </View>
   );
-}
+};
+
+export default LoaderScreen;
 
 const styles = StyleSheet.create({
-  container: {
+  Container: {
     flex: 1,
-    backgroundColor: '#F5EEDC',
-    alignItems: 'center',
+    backgroundColor: '#3949AB',
     justifyContent: 'center',
-    paddingHorizontal: 12
-  },
-  inputContainer: {
-    backgroundColor: 'blue',
-    width: '100%',
-    borderRadius: 8,
-    flexDirection: 'row',
     alignItems: 'center',
-    borderWidth: 4,
-    borderColor: '#d7d7d7'
   },
-  inputField: {
-    padding: 14,
-    fontSize: 22,
-    width: '90%',
-    color: 'red'
-  }
+  Logo: {
+    color: 'white',
+    fontSize: 35,
+    fontFamily: 'Lato',
+  },
 });
